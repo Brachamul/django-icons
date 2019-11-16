10f3b5cdafd1af151cee2355ff3a4242406c574f
Dylan Verheul <dylan@zostera.nl>
2019-11-16 07:22:41 +0100

    Mark development status stable

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     zip_safe=False,
     keywords="django-icons",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
